3db757a8d7487cb12b5e8108e591f71258334437
Rúni Eriksen <runi.eriksen@teledyneflir.com>
2022-04-21 12:40:49 +0200

    [HABA-593] Remove lp5562 init sequence
    * Remove LED driver chip init sequence so that leds aren't
      reset and turned off.
    * Chip is configured in u-boot.

--- drivers/leds/leds-lp5562.c
+++ drivers/leds/leds-lp5562.c
@@ -548,10 +548,6 @@ static int lp5562_probe(struct i2c_client *client,
 
 	i2c_set_clientdata(client, led);
 
-	ret = lp55xx_init_device(chip);
-	if (ret)
-		goto err_init;
-
 	ret = lp55xx_register_leds(led, chip);
 	if (ret)
 		goto err_register_leds;
