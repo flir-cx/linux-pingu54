f519fed7e31df9e46169dcabd63fcbb6567c354d
Adrian Marklund <adrian.marklund@teledyneflir.com>
2023-05-22 13:43:01 +0200

    dt-bindings: tlv320aic31xx: Digital mic settings
    Options for configuring a digital mono microphone. [BC-429]
    
    Signed-off-by: Adrian Marklund <adrian.marklund@teledyneflir.com>

--- include/dt-bindings/sound/tlv320aic31xx-micbias.h
+++ include/dt-bindings/sound/tlv320aic31xx-micbias.h
@@ -2,8 +2,13 @@
 #ifndef __DT_TLV320AIC31XX_MICBIAS_H
 #define __DT_TLV320AIC31XX_MICBIAS_H
 
+#define MICBIAS_OFF		0
 #define MICBIAS_2_0V		1
 #define MICBIAS_2_5V		2
 #define MICBIAS_AVDDV		3
+
+#define MIC_ANALOG              0
+#define MIC_DIGITAL_GPIO1       1
+#define MIC_DIGITAL_DIN         2
 
 #endif /* __DT_TLV320AIC31XX_MICBIAS_H */
